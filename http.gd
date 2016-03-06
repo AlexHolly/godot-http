6397818258fd9b45d7b8797fa762b0ea3ee2bc16
Alexander Holland <alexander.holland@live.de>
2016-03-06 20:10:01 +0100

    put body support

--- http.gd
+++ http.gd
@@ -1,61 +1,55 @@
-#by alexholly
-
-extends Node2D
+#author https://github.com/AlexHolly
+extends Node
 
 var headers=[
 	"User-Agent: Pirulo/1.0 (Godot)",
 	"Accept: */*"
 ]
 
+var HTTP = "http://"
 #TODO http request liefert die response, sollten die errors abgefangen werden?
 #TODO Was machen mit players wie werden sie geupdated siehe board get players problem
 #TODO Asynchrone anfragen einbauen? Etwas komplizierter für anfragenden, muss call back funktion mit geben?
-
-var http = HTTPClient.new() # Create the Client
-#var serverAdress
-#var port 
+#TODO warpper/function für verbindungsaufbau
+#TODO http nicht connected abfrangen?
 
 func _init():
 	pass
 	
 func _ready():
-	#serverAdress = get_node("/root/global").get_ha_proxy()
-	#port = get_node("/root/global").get_ha_proxy_port()
-	#http.set_blocking_mode( true )
 	pass
 
 func get(adress):
-	#print(adress)
 	var uri_dict = get_link_address_port_path(adress)
-	print("SENDING NEW REQUEST")
-	checkServerConnection(uri_dict["host"], uri_dict["port"])
+	var http = checkServerConnection(uri_dict["host"], uri_dict["port"])
+	
+	# http nicht connected abfrangen?
 	http.request(HTTPClient.METHOD_GET, adress.percent_encode(), headers)
-	return getResponse()
+	return getResponse(http)
 	
-func put(adress):
-	
+func put(adress,body=""):
 	var uri_dict = get_link_address_port_path(adress)
-	
-	checkServerConnection(uri_dict["host"], uri_dict["port"])
-	http.request(HTTPClient.METHOD_PUT, adress.percent_encode(), headers)
-	
-	return getResponse()
+	var http = checkServerConnection(uri_dict["host"], uri_dict["port"])
+
+	# http nicht connected abfrangen?
+	http.request(HTTPClient.METHOD_PUT, adress.percent_encode(), headers, body)
+	return getResponse(http)
 	
 func delete(adress):
 	var uri_dict = get_link_address_port_path(adress)
-	
-	checkServerConnection(uri_dict["host"], uri_dict["port"])
+	var http = checkServerConnection(uri_dict["host"], uri_dict["port"])
+
+	# http nicht connected abfrangen?
 	http.request(HTTPClient.METHOD_DELETE, adress.percent_encode(), headers)
+	return getResponse(http)
 	
-	return getResponse()
-	
-func post(adress, body):
+func post(adress, body=""):
 	var uri_dict = get_link_address_port_path(adress)
-	
-	checkServerConnection(uri_dict["host"], uri_dict["port"])
+	var http = checkServerConnection(uri_dict["host"], uri_dict["port"])
+
+	# http nicht connected abfrangen?
 	http.request(HTTPClient.METHOD_POST, adress.percent_encode(), headers, body)
-	
-	return getResponse()
+	return getResponse(http)
 	
 func get_link_address_port_path(uri):
 	var link = uri.replace("http://", "")
@@ -70,11 +64,9 @@ func get_link_address_port_path(uri):
 		port = adress_port[1]
 	
 	var path = uri.replace(adress,"")
-	#print(adress)
-	path = uri.replace("http://"+adress+":"+port,"")
-	#print(str("REQUEST:", path))
-	
-	#print(str("REQUEST:", path))
+
+	path = uri.replace(HTTP+adress+":"+port,"")
+
 	#print("request: " + path)
 	return {
 			"uri":uri, 
@@ -86,66 +78,79 @@ func get_link_address_port_path(uri):
 			}
 
 func checkServerConnection(serverAdress,port):
-	var err = http.connect(serverAdress,port) # Connect to host/port
-
-	while( http.get_status()==HTTPClient.STATUS_CONNECTING or http.get_status()==HTTPClient.STATUS_RESOLVING):
-		http.poll()
+	var http = HTTPClient.new() # Create the Client
 	
+	http.set_blocking_mode( true ) #wait untl all data is available on response
+	var err = http.connect(serverAdress,port) # Connect to host/port
+	#print(err)
+	
+	if(!err):
+		#var try = 0
+		#try>= 10 &&
+		while( http.get_status()==HTTPClient.STATUS_CONNECTING or http.get_status()==HTTPClient.STATUS_RESOLVING):
+			http.poll()
+			#try+=1
+	return http
 
-func getResponse():
+
+func getResponse(http):
 
 	var rs = {}
+	#null or {}?
 	rs["header"] = {}
+	rs["body"] = null
 	# Keep polling until the request is going on
 	while (http.get_status() == HTTPClient.STATUS_REQUESTING):
 		http.poll()
+		#print("read header")
 		#print("ich frage die datei an...")
 	
 	# If there is header content
 	if (http.has_response()):
 		# Get response headers
 		var headers = http.get_response_headers_as_dictionary()
-		
 		for key in headers:
 			rs["header"][key.to_lower()] = headers[key]
+
 		#print(rs)
 		var cache = headers
 		rs["code"] = http.get_response_code()   
-	
-		 #This method works for both anyway
+		#This method works for both anyway
 		var rb = RawArray() #array that will hold the data
 		
-		if(http.get_status()==HTTPClient.STATUS_BODY):
+		while(http.get_status()==HTTPClient.STATUS_BODY):
 			http.set_read_chunk_size( http.get_response_body_length() )
-			var rb = http.read_response_body_chunk()
+			rb += http.read_response_body_chunk()
 			#print(rs)
-			if("content-length" in rs["header"]):
-				rs["body"] = parse_body_to_var(rb, rs["header"]["content-type"])
-				#print(rs)
-			else:
-				rs["body"] = null
-			return rs
+			
+		if("content-length" in rs["header"]):
+			print(str("EMPFANGEN LENGHT:", rs["header"]["content-length"]))
+			rs["body"] = parse_body_to_var(rb, rs["header"]["content-type"])
+			#print(rs)
 		else:
-			print("chunked transfer not supported")
-			#while(http.get_status()==HTTPClient.STATUS_BODY):
-			#	# While there is body left to be read
-			#	http.poll()
-			#	# Get a chunk
-			#	var chunk = http.read_response_body_chunk()
-			#	rb = rb + chunk # append to read bufer
+			rs["body"] = null
+			#print("maybe chunked or error? chunked transfer not supported")
+		#print("http empfangen")
+		return rs
 	else:
-		print("no response")
+		print("http.gd - no response")
+		pass
 
 func parse_body_to_var(body, content_type):
 	
 	if(content_type.find("application/json")!=-1):
 		
 		var bodyDict = {}
-		body = body.get_string_from_utf8()
+		body = body.get_string_from_ascii()
+		
+		#print(body)
 		
 		if( bodyDict.parse_json( body ) == 0 ):
 			body = bodyDict
-			
+			print("make dict")
+		else:
+			print("Error beim body to dict json wandel")
+		
 	elif(content_type.find("bytestream")!=-1):
 		
 		pass#return body
